c01dd26891d9ae279056d1749adb8b7e554a046c
Santiago Arango Gutierrez <santiago.arango@arquitectosdenegocios.com>
2021-03-13 10:32:59 -0500

    Create app get rick and morthy api in Javascript and Html

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+// Consulta de API Character
+getText();
+
+async function getText() {
+    const url = 'https://rickandmortyapi.com/api/character/';
+    fetch(url)
+        .then((resp) => resp.json())
+        .then(function(data) {
+            let info = data.results;
+            return info.map(function(personaje) {
+                if (personaje.status == 'Alive') {
+                    statusChar = `<span class="badge bg-success">${personaje.status}</span>`;
+                } else {
+                    statusChar = `<span class="badge bg-danger">${personaje.status}</span>`;
+                }
+                const html = `
+                <div class="col">
+                    <div class="card">
+                        <img src="${personaje.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${personaje.name} ${statusChar}</h5>
+                            <p class="card-text">
+                            <ul>
+                            <li><strong class="text-info">Especie:</strong> ${personaje.species}</li>
+                            <li><strong class="text-info">Genero:</strong> ${personaje.gender}</li>
+                            </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                `;
+                content.innerHTML += html;
+                console.log(personaje);
+            })
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
